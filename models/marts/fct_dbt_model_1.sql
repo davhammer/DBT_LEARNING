c8baec5aa0103bb181340ac5b4a655408788e998
David Hammer <davhammer@gmail.com>
2024-05-23 15:08:35 +0000

    Change 3

---
+++ models/marts/fct_dbt_model_1.sql
@@ -0,0 +1,4 @@
+
+select *
+from {{ ref('stg_dbt_model_1') }}
+where id = 1
---
+++ models/staging/stg_dbt_model_1.sql
@@ -0,0 +1,9 @@
+with source_data as (
+
+    select 1 as id
+    union all
+    select 2 as id
+
+)
+
+select * from source_data
